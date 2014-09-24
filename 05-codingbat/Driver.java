85339498b650a3a58a554bfc1c203bd7d3de7534
Emilyxu828 <Emilyxu828@hotmail.com>
2014-09-23 22:37:05 -0400

    Committed driver.java

---
+++ 05-codingbat/Driver.java
@@ -0,0 +1,14 @@
+public class Driver {
+	public static void main(String[] args){
+		stringstuff what = new stringstuff();
+
+		System.out.println(what.nonStart("hello", "mate"));
+
+		System.out.println(what.makeAbba("love", "pizza"));
+
+		System.out.println(what.diff21(54));
+
+	}
+}
+
+
