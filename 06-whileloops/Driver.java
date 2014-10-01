456e7373b758ce2b443d4483e150f890dd5c2bfe
Emilyxu828 <Emilyxu828@hotmail.com>
2014-09-30 21:05:52 -0400

    Committed Driver.java

---
+++ 06-whileloops/Driver.java
@@ -0,0 +1,9 @@
+public class Driver {
+  public static void main(String[] args){
+    whileloops jello = new whileloops();
+    System.out.print(jello.frontTimes("whatsup", 84));
+    System.out.print(jello.stringBits("qwertyuiopjh"));
+    System.out.print(jello.stringYak("yakahakamakatakayakyakyakkajagya"));
+    System.out.print(jello.stringMatch("qqooffhjjiwhhree","qqaoffhdjimhlree"));
+  }
+}
