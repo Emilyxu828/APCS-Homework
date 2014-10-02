5395b39907be374db169fc638c2f811a3e626faa
Emilyxu828 <Emilyxu828@hotmail.com>
2014-10-01 23:31:34 -0400

    Committed Shapes.java

--- 07-shapes/Shapes.java
+++ 07-shapes/Shapes.java
@@ -31,26 +31,36 @@ public class Shapes {
 	    a = a + 1;
 	}
 	return stars;
-   }
+    }
 
 
-   public String tri2(int h){ 
-        int a = 0;
-	int b = 0;
+    public String tri2(int h){ 
+        int a = 1;
+	int b = 1;
+        int c = h;
 	String stars = "";
-	while (a < n) {
+	while (a <= h) {
 	    b = a;
+            c = h - a;
+            while (c > 0) {
+                stars = stars + " ";
+                c = c-1;
+            }
 	    while (b > 0) {
-		stars = stars + " ";
+                stars = stars + "*";
 		b = b - 1;
-    	    }	
-	    if (b = 0) {
-		stars = stars + "*";
-		b = b - 1;
-	    }
+            }	
 	    stars = stars + "\n";
 	    a = a + 1;
 	}
 	return stars;
-   }
+
+/*
+_ _ _ _ *     a=1, b=1, c=4             n = 5
+_ _ _ * *     a=2, b=2, c=3
+_ _ * * *
+_ * * * *
+* * * * *
+*/
+    }
 }
