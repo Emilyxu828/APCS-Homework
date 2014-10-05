7b610e103b878e90f602f239633206f50387f6eb
Emilyxu828 <Emilyxu828@hotmail.com>
2014-10-04 23:48:55 -0400

    Committed Driver.java

---
+++ 08-/shapes2/Driver.java
@@ -0,0 +1,7 @@
+public class Driver {
+  public static void main(String[] args){
+    Shapes2 qwerty = new Shapes2();
+    System.out.println(qwerty.frame(14,15));
+    System.out.println(qwerty.frame(3,4));
+  }
+}
