3e166a952dc28621c413f542d3236d31702a7f94
Emily Xu <emilyxu828@hotmail.com>
2014-10-08 08:29:05 -0400

    Committed Driver.java

---
+++ APCS-Classwork/10-1-14/Driver.java
@@ -0,0 +1,7 @@
+public class Driver {
+    public static void main(String[] args){
+      	Box boxy = new Box();
+	System.out.println(boxy.box(2,9));
+	System.out.println(boxy.box(12,12));
+    }
+}
