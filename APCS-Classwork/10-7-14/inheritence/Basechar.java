3599bd3f3328fd52b72bbe96438b7a46c9fc12c8
Emily Xu <emilyxu828@hotmail.com>
2014-10-09 08:05:39 -0400

    Committed Basechar.java

---
+++ APCS-Classwork/10-7-14/inheritence/Basechar.java
@@ -0,0 +1,14 @@
+// superclass of Warrior
+
+public class Basechar {
+    private int health=20;
+
+    public int getHealth() {
+	return health;
+    }
+
+    public void setHealth(int i){
+	health = i;
+    }
+
+}
