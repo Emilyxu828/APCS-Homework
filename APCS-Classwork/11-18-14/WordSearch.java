65e51fa28cdafce71af351918cbe45a73714d334
Emily Xu <emilyxu828@hotmail.com>
2014-11-19 08:41:28 -0500

    Committed

--- APCS-Classwork/11-18-14/WordSearch.java
+++ APCS-Classwork/11-18-14/WordSearch.java
@@ -11,12 +11,12 @@ public class WordSearch{
 	board = new char[r][c];
 	for (int i=0; i<board.length; i++){
 	    for (int j=0; j<board[i].length; j++){
-		board[i][j]=".";
+		board[i][j]='.';
 	    }
 	}
     }
     public WordSearch(){
-	WordSearch(30,20);
+	this(20,30);
     }
 
     public String toString(){
@@ -32,15 +32,30 @@ public class WordSearch{
 
     public void addWordH(String w, int row, int col){
 	int r  =row, c = col;
-	for (int i=0; i<w.length(); i++){
-	    for (int j=0; i<board.length; i++){
-		board[i][j]='.';
+	if (row>board.length || col>board[1].length)
+	    System.out.println("Row or Column is too big"); 
+	if (board[1].length-col <= w.length()){
+	    for (int i=0;i<w.length();i++){
+		board[r][c] = w.charAt(i);
+		c++;
 	    }
 	}
+	else 
+	    System.out.println("Can't place word here");
+	
     }
 
 
 
     public static void main (String[] args){
+	WordSearch w = new WordSearch();
+       	System.out.println(w);
+	w.addWordH("hello",3,5);
+       	w.addWordH("look",3,8);
+       	w.addWordH("look",3,5);
+        w.addWordH("hello",100,5);
+      	w.addWordH("hello",30,555);
+				
+	System.out.println(w);
     }
 }
