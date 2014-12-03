53e15e1290ad9592cacd08053ddd25170b48c6c6
Emilyxu828 <Emilyxu828@hotmail.com>
2014-12-02 23:53:37 -0500

    Committed

---
+++ APCS-Classwork/11-26-14/OrderedSuperArray.java
@@ -0,0 +1,20 @@
+public class OrderedSuperArray extend Sarray {
+
+    //data is an array! OrderedSuperArray?!
+
+    public void add(String e){
+	String thisword;
+	for (int i=0; i<OrderedSuperArray.size(); i++){
+	    thisword = OrderedSuperArray.get(i);
+	    if (int(e[i]<int(thisword[i])
+Ordered
+	    break;
+	    for (int q=0; q<thisword.get(i).length(); q++){
+		if (int(e[q])<int(thisword[q])     //e[i].compareTo(thisword[i])
+
+
+    }
+
+    public static void main (String[] args){
+    }
+}
