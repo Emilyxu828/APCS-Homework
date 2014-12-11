94039e6dad33033903ac42e0b22112d35c37b377
Emily Xu <emilyxu828@hotmail.com>
2014-12-11 08:41:06 -0500

    Committed Search.java

---
+++ APCS-Classwork/12-11-14/Search.java
@@ -0,0 +1,16 @@
+public class Search{
+    private String[] a = new String[20];
+
+    public String lsearch(){       //linear search
+    }
+
+    public String bsearch(){       //binary search
+	int mid = a.length/2;
+	int low = 0;
+	int high = a.length;
+    }
+
+    public String rsearch(){       //recursive binary search
+    }
+
+}
